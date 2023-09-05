7a56492639fd19f18f0d9ba46fd1277781ec412e
ArcherHuang <mmosconii@gmail.com>
2023-09-05 15:27:34 +0800

    Deploy Code to Production

---
+++ index.js
@@ -0,0 +1,21 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+
+// GET /
+app.get('/', async (req, res) => {
+    res.send('Hello World ooo');
+})
+
+// GET /pingpong
+app.get('/pingpong', async (req, res) => {
+    res.send(process.env.info);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
